d6d0101d4cb5c502f27790bb85be3f48d788614c
Patrick Lai <weitanai@pinduoduo.com>
2020-03-14 11:37:49 +0800

    test(*): add fake test

---
+++ tests/App.test.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { render } from "@testing-library/react";
+// import App from "./App";
+
+test("fake test", () => {
+  // const { getByText } = render(<App />);
+  // const linkElement = getByText(/trigger travis/i);
+  // expect(linkElement).toBeInTheDocument();
+  expect(true).toBe(true);
+});
